30ca92f9b234f6c623b878eedbb3de324896510e
artem-telnov <telnowtema@mail.ru>
2023-03-24 17:41:13 +0300

    add solution search_a_2d_matrix

--- solutions/search_a_2d_matrix/solution/solution.py
+++ solutions/search_a_2d_matrix/solution/solution.py
@@ -3,4 +3,28 @@ from typing import List
 
 class Solution:
     def solution(self, matrix: List[List[int]], target: int) -> bool:
-        pass
+        for row in matrix:
+            if row[0] <= target <= row[-1]:
+                # Если target между первым и последним, 
+                # значит мы нашли нужную нам строку.
+                return self.binary_search(row, target)
+        return False
+
+    def binary_search(self, nums: List[int], target: int) -> bool:
+        """
+        Классический бинарный поиск. 
+        О нем можно почитать в задаче binary_search.
+        """
+        left, rigth = 0, len(nums) -1
+
+        while left <= rigth:
+            mid = (left + rigth) // 2
+
+            if target == nums[mid]:
+                return True
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                rigth = mid - 1
+
+        return False
